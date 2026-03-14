9411828efcfa5d095077f045fc664f35b251d7c8
AvaLilac <amyshimplays@gmail.com>
2026-03-13 20:51:24 -0400

    Redoes the plugin repo

--- pluginrepobackend.js
+++ pluginrepobackend.js
@@ -1,25 +1,19 @@
 {
   "plugins": [
     {
-      "name": "Write Uppercase",
+      "name": "WriteUpperCase",
       "author": "AvaLilac",
       "link": "https://cdn.jsdelivr.net/gh/AvaLilac/Ava-s-AviaClient-Plugins@refs/heads/main/WriteUpperCase.js",
-      "description": "Auto-capitalizes sentences while you type."
+      "description": " Auto-capitalizes sentences while you type."
     },
     {
       "name": "MonacoCSS",
       "author": "AvaLilac",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/QuickCSSPlus.js",
-      "description": "Enhanced QuickCSS editor with themes."
+      "description": "Enhances Avia’s QuickCSS with the Monaco editor, delivering real syntax highlighting, instant live updates, and a smoother editing experience."
     },
     {
-      "name": "Userbadges",
-      "author": "AvaLilac",
-      "link": "https://raw.githubusercontent.com/AvaLilac/AviaClientBadges/refs/heads/main/userbadges.js",
-      "description": "Adds custom user badges to profiles."
-    },
-    {
-      "name": "Force Apperance",
+      "name": "ForceApparence",
       "author": "AvaLilac",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/ForceApparence.js",
       "description": "Keeps the Appearance label in English so Avia Client settings continue working properly."
@@ -29,6 +23,60 @@
       "author": "AvaLilac",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/Translate.js",
       "description": "Translates your messages and other users messages into whatever language you choose"
+    },
+    {
+      "name": "Timezones",
+      "author": "AvaLilac",
+      "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/timezones.js",
+      "description": "Now set custom timezones!"
+    },
+    {
+      "name": "CtrlEnterSend",
+      "author": "AvaLilac",
+      "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/CtrlEnterSend.js",
+      "description": "Sends message only when you press CtrlEnter And creates a new line when you press enter"
+    },
+    {
+      "name": "ForceEmojiPack",
+      "author": "0simp",
+      "link": "Forces emojis in embeds and message reactions to use your selected emoji pack",
+      "description": "https://raw.githubusercontent.com/0simp/ForceEmojiPack/refs/heads/main/ForceEmojiPack.js"
+    },
+    {
+      "name": "AppleEmojiPack",
+      "author": "AvaLilac",
+      "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/AppleEmoji.js",
+      "description": "Converts every stoat emoji to Apples"
+    },
+    {
+      "name": "ShrinkEmojis",
+      "author": "0simp",
+      "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/ShrinkEmojis.js",
+      "description": "It shrinks emojis 🤯"
+    },
+    {
+      "name": "CustomHomeButton",
+      "author": "0simp",
+      "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/CustomHomeButton.js",
+      "description": "Allows you to set a custom home button image (look in the AviaClient Server to know what css to use)"
+    },
+    {
+      "name": "Userbadges",
+      "author": "AvaLilac",
+      "link": "https://raw.githubusercontent.com/AvaLilac/AviaClientBadges/refs/heads/main/userbadges.js",
+      "description": "Plugin loads custom badges AvaLilac Puts in the backend on any user"
+    },
+    {
+      "name": "CopyAttachmentLink",
+      "author": "0simp",
+      "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/CopyAttachmentLink.js",
+      "description": "Adds a button to non-image/video attachments to copy the attachment link"
+    },
+    {
+      "name": "AddToFavourites",
+      "author": "0simp",
+      "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/AddToFavourites.js",
+      "description": "Adds a button to the image viewer to add the image to favourites. Currently no support for adding a custom title because I'm too stupid to figure out how to do it but I might add it in the future. Also for some reason the image only shows up in favourites after you refresh, no fucking clue why that is."
     }
   ]
 }
