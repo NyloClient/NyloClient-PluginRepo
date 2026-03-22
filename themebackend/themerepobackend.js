dfe128586d97b0dd882e1878ccb46e1ad1292db9
AvaLilac <amyshimplays@gmail.com>
2026-03-21 23:21:12 -0400

    Update themerepobackend.js

--- themebackend/themerepobackend.js
+++ themebackend/themerepobackend.js
@@ -3,6 +3,10 @@
     {
       "name": "Avalilac's Themes",
       "url": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/themesbackend.js"
+    },
+    {
+      "name": "Raptor's Themes",
+      "url": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/themesbackend.js"
     }
   ]
 }
