43cb6bd003e9584151054ff6bbc77ae9719eb767
AvaLilac <257690424+AvaLilac@users.noreply.github.com>
2026-04-21 20:49:09 -0400

    add a few morep lugins including stoat server
    also added the username tags to the end of the author

--- pluginrepobackend.js
+++ pluginrepobackend.js
@@ -2,199 +2,227 @@
   "plugins": [
     {
       "name": "WriteUpperCase",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://cdn.jsdelivr.net/gh/AvaLilac/Ava-s-AviaClient-Plugins@refs/heads/main/WriteUpperCase.js",
       "description": " Auto-capitalizes sentences while you type.",
       "icon": "spellcheck"
     },
     {
       "name": "ForceApparence",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/ForceApparence.js",
       "description": "Keeps the Appearance label in English so Avia Client settings continue working properly.",
       "icon": "palette"
     },
     {
       "name": "Translate",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/Translate.js",
       "description": "Translates your messages and other users messages into whatever language you choose",
       "icon": "translate"
     },
     {
       "name": "Timezones",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/timezones.js",
       "description": "Now set custom timezones!",
       "icon": "schedule"
     },
     {
       "name": "CtrlEnterSend",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/CtrlEnterSend.js",
       "description": "Sends message only when you press CtrlEnter And creates a new line when you press enter",
       "icon": "keyboard"
     },
     {
       "name": "ForceEmojiPack",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/ForceEmojiPack/refs/heads/main/ForceEmojiPack.js",
       "description": "Forces emojis in embeds and message reactions to use your selected emoji pack",
       "icon": "emoji_emotions"
     },
     {
       "name": "AppleEmojiPack",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/AppleEmoji.js",
       "description": "Converts every stoat emoji to Apples",
       "icon": "emoji_emotions"
     },
     {
       "name": "ShrinkEmojis",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/ShrinkEmojis.js",
       "description": "It shrinks emojis 🤯",
       "icon": "emoji_emotions"
     },
     {
       "name": "CustomHomeButton",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/CustomHomeButton.js",
       "description": "Allows you to set a custom home button image (look in the AviaClient Server to know what css to use)",
       "icon": "home"
     },
     {
       "name": "Userbadges",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/AviaClientBadges/refs/heads/main/userbadges.js",
       "description": "Plugin loads custom badges AvaLilac Puts in the backend on any user",
       "icon": "manage_accounts"
     },
     {
       "name": "CopyAttachmentLink",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/CopyAttachmentLink.js",
       "description": "Adds a button to non-image/video attachments to copy the attachment link",
       "icon": "attach_file"
     },
     {
       "name": "BetterFavourites",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/BetterFavourites.js",
       "description": "Improves avia's built in favourites system by adding a button to the image viewer to add the image to favourites, a refresh button to the favourites panel (click to make images/gifs added via the add favourites button show up) and making clicking on an image/gif put the link into the chat bar instead of copying it to the clipboard. This plugin is just a combination of AddToFavourites,FavsToChat and a new refresh button.",
       "icon": "bookmark"
     },
     {
       "name": "Avia Server Button",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/aviaserverbutton.js",
       "description": "Changes the Go to stoat server to avia's",
       "icon": "home"
     },
     {
       "name": "Masquerade",
-      "author": "AvaLilac x 0simp",
+      "author": "AvaLilac#7432 x 0simp#2291",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/masquerade.js",
       "description": "Exactly what the name says. Allows you to change your pfp/avatar before sending a message without modifying your profile. has the ability to impersonate anyone by clicking onto their profile, clicking the 3 dots. Then clicking impersonate.",
       "icon": "theater_comedy"
     },
     {
       "name": "FavsToChat",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/FavsToChat.js",
       "description": "Makes clicking an image/gif in the favourites panel put the link in the chat bar instead of copying it to the clipboard",
       "icon": "bookmark"
     },
     {
       "name": "Gradient roles",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/GradientRoles.js",
       "description": "Create Roles with gradients or modify existing roles to add gradients    WARNING THIS IS A TOKEN GRABBER When creating roles you need to provide your user token or else you aren't able to create a role THIS plugin grabs the token for you so you dont have to grab it every time.    If you do not feel comfortable Using this plugin because it grabs your token. Then you can either look through the code itself as its open source. Or just don't use it in general",
       "icon": "gradient"
     },
     {
       "name": "Headliner",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/headliner.js",
       "description": "DESKTOP ONLY. The Plugin version of the headliner CSS.Found In Desktop settings. You can modify Its settings by just clicking that gear icon on the button",
       "icon": "wallpaper"
     },
     {
       "name": "EmojiSounds",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/EmojiSounds.js",
       "description": "Allows you to set sounds to be played when certain emojis are sent in chat. After enabling the plugin, go into your user settings and click on the button that says '(Avia) Emoji Sounds Settings'. Then, paste the emoji and the direct link to the sound you want in the appropriate boxes.",
       "icon": "volume_up"
     },
     {
       "name": "Devtools",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/ConsoleDevTools.js",
       "description": "Get Full access to the browsers devtools console inside AviaClientDesktop \"works on web\"",
       "icon": "developer_mode"
     },
     {
       "name": "NoBlockedMessages",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/NoBlockedMessages.js",
       "description": "Completely Hides blocked messages. the messages that says '1 blocked message' hides them from even showing up",
       "icon": "block"
     },
     {
       "name": "NoSmoothScroll",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/NoSmoothScroll.js",
       "description": "Completely hides the messages from showing",
       "icon": "swap_vert"
     },
     {
       "name": "ChunkyMembers",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/ChunkyMembers.js",
       "description": "Makes the member and pinned message lists cover the chat entirely. I recommend combining it with the following css found inside the AviaClient Server",
       "icon": "people"
     },
     {
       "name": "GifFix",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/GifFix.js",
       "description": "Fixes gifs and animated emojis not animating when they should",
       "icon": "gif_box"
     },
     {
       "name": "StoatDupeFix",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/stoatDupeFix.js",
       "description": "tries its best to fix the duped embeds/images glitch stoat seems to be having Not 100% but it tries its best",
       "icon": "build"
     },
     {
       "name": "HideImageLinks",
-      "author": "0simp",
+      "author": "0simp#2291",
       "link": "https://raw.githubusercontent.com/0simp/AviaPlugins/refs/heads/main/HideImageLinks.js",
       "description": "Hides image links if they're the only content in the message",
       "icon": "hide_image"
     },
     {
       "name": "NotificationPings",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/NotificationPings.js",
       "description": "Adds the ability to be pinged whenever you are either dmed or mentioned",
       "icon": "notifications"
     },
     {
       "name": "TimezoneV2",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/timezonesV2",
       "description": "Now set custom time zones! V2 Now including PerUser Time zones.  You can also disable Global Time zone and just have PerUser",
       "icon": "schedule"
     },
     {
       "name": "CharacterCounter",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "link": "https://raw.githubusercontent.com/AvaLilac/Ava-s-AviaClient-Plugins/refs/heads/main/CharacterCounter.js",
       "description": "Add a counter to your Textbox to display how many letters till you hit stoats limit inside channels",
       "icon": "calculate"
+    },
+    {
+      "name": "Copy to clipboard",
+      "author": "AvaLilac#7432",
+      "link": "https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/copytoclipboard.js",
+      "description": "Copy images to clipboard",
+      "icon": "paste"
+    },
+    {
+      "name": "BanUserWithoutBeingInServer",
+      "author": "AvaLilac#7432",
+      "link": "https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/BanUserWithoutBeingInServer.js",
+      "description": "Ban users even if they arent in your server. Useful when you see users be really bad in other servers and you rather it not be in your server. Stop it before it happens",
+      "icon": "block"
+    },
+    {
+      "name": "Timestamps",
+      "author": "AvaLilac#7432",
+      "link": "https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/Timestamps.js",
+      "description": "Easily make custom timestamps!",
+      "icon": "timer"
+    },
+    {
+      "name": "NoAnnoyingToolTips",
+      "author": "0simp#2291",
+      "link": "https://codeberg.org/0simp/AviaPlugins/raw/branch/main/NoAnnoyingTooltips.js",
+      "description": "Removes annoying tooltips (eg toggle main sidebar)",
+      "icon": "people"
     },
     {
       "name": "StoatServers.com Discovery",
